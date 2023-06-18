f3d66b7e878cd2fe77d0ff8e2b1133ecc2207363
Timur Ishmuratov <ishmuratov7timur7@gmail.com>
2023-06-18 19:19:08 +0100

    Added custom benchmark

--- src/benchmark.py
+++ src/benchmark.py
@@ -39,11 +39,12 @@ class Benchmark:
     def get_existing_benchmarks(self) -> Dict[Str, List[Tuple[str, int]]]:
         pass
 
-    def run(self, task: str, metrics: List[str]):
+    def run(self, task: str, metrics: List[str], custom_metric = None):
         """
         :task: ['regression', 'classification']
         :metrics: Available classification metrics: ['accuracy', 'precision', 'recall', 'f1_score']
                   Available regression metrics: ['mae', 'mse', 'rmse', 'r2']
+        :custom_metric: your python function to calculate a metric of your preference
         """
         predictions = self._callback(self._dataset_info)
         targets = self._dataset.targets
@@ -61,10 +62,44 @@ class Benchmark:
         for metric in metrics:
             results.append((metric, metric_calculator.calculate_metric(metric))
 
+        if custom_metric:
+            results.append((custom_metric.__name__, custom_metric(predictions, targets)))
+
         return results
 
-        print(user_model_result)
-        # actual benchmark here
+    
+class CustomBenchmark:
+    """
+    Benchmark on a custom dataset on a single or multiple models
+    """
+
+    def __init__(self, features: List[any], targets: List[any]):
+        self._features = features
+        self.targets = targets
+
+    def run(model: any, metrics: List[str], custom_metric = None):
+        """
+        :task: ['regression', 'classification']
+        :metrics: Available classification metrics: ['accuracy', 'precision', 'recall', 'f1_score']
+                  Available regression metrics: ['mae', 'mse', 'rmse', 'r2']
+        :custom_metric: your python function to calculate a metric of your preference
+        """
+
+        if task == 'regression':
+            metric_calculator = RegressionMetrics(predictions, targets)
+        elif task  == 'classification':
+            metric_calculator = ClassificationMetrics(predictions, targets)
+        else:
+            raise NotSupportedTask('This kind of task is not currently supported, use Benchmark.run_custom(data, metric_function)')
+
+        results = []
+        for metric in metrics:
+            results.append((metric, metric_calculator.calculate_metric(metric))
+
+        if custom_metric:
+            results.append((custom_metric.__name__, custom_metric(predictions, targets)))
+
+        return results
 
 
 if __name__ == '__main__':
---
+++ tests/test_benchmark.py
@@ -0,0 +1,3 @@
+from typing import Callable, List, Tuple, Dict
+import pytest
+from src import AutoBenchmark, CustomBenchmark
