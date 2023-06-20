add4ce4b6a6bb4c56d5b64af10490814e3fd97d2
Timur Ishmuratov <ishmuratov7timur7@gmail.com>
2023-06-20 19:06:10 +0100

    Setup.py updated

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='aibenchmark',
+    version='0.0.1',
+    license='MIT',
+    author="Based Labs",
+    author_email='',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/BasedLabs/aibenchmark/',
+    keywords='ai benchmark metrics',
+    install_requires=[
+          'scikit-learn',
+          'torch',
+          'py7zr',
+          'gdown',
+          'pytest',
+          'numpy',
+          'tensorflow',
+      ],
+)
