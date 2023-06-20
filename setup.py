78ff1e83b9c71bf96dce4e3d99355cb1c38cf11a
Timur Ishmuratov <ishmuratov7timur7@gmail.com>
2023-06-20 22:50:05 +0100

    Changed setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     license='MIT',
     author="Based Labs",
     author_email='',
-    packages=find_packages(),
+    packages=find_packages('src/aibenchmark'),
     package_dir={'': 'src'},
     url='https://github.com/BasedLabs/aibenchmark/',
     keywords='ai benchmark metrics',
