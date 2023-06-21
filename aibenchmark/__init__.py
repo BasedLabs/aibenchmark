e2de75af12e9fa5fc883894f7b08dfdfec46ee50
JaktensTid <jaktenstid1@gmail.com>
2023-06-21 20:39:21 +0300

    fix(pip): Added correct folders

--- aibenchmark/__init__.py
+++ aibenchmark/__init__.py
@@ -1,5 +1,6 @@
 import logging
-#from aibenchmark import benchmark
-#from aibenchmark import metrics
+from aibenchmark import benchmark
+from aibenchmark import metrics
+from aibenchmark import dataset
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
