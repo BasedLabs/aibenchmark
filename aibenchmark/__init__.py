6d6b9e74cae5353b71d8aac154f6b05ad93cb8da
JaktensTid <jaktenstid1@gmail.com>
2023-06-21 20:18:30 +0300

    fix(pip): Fixing pip

--- aibenchmark/__init__.py
+++ aibenchmark/__init__.py
@@ -1,3 +1,5 @@
 import logging
+from benchmark import CustomBenchmark, Benchmark, DatasetBase, DatasetInfo, CustomDataset, BenchmarkData
+from metrics import RegressionMetrics, ClassificationMetrics
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
