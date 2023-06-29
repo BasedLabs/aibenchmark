4e3f4be1629917794e12c58f08d0c09da382860c
Timur Ishmuratov <ishmuratov7timur7@gmail.com>
2023-06-29 20:21:15 +0100

    New version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aibench',
-    version='0.0.4',
+    version='0.0.5',
     license='MIT',
     author="Based Labs",
     author_email='',
